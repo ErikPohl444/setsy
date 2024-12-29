b68fcc42a75c97581f6df8fe5b43ae196a737540
Erik Pohl <epohl@dmigs.com>
2024-12-28 22:51:13 -0500

    added tests

--- tests/test_setsy.py
+++ tests/test_setsy.py
@@ -21,3 +21,17 @@ def test_powerset():
             set(n) for n in expected
         ]
     assert len(y) == len(expected)
+
+def test_is_not_subset():
+    s1 = setsy([1,2,3])
+    s2 = setsy([1,2,4,5])
+    assert s1.is_not_subset(s2)
+
+def test_is_not_superset():
+    s1 = setsy([1,2,3])
+    s2 = setsy([1,2,4,5])
+    assert s2.is_not_superset(s1)
+
+def test_preserve_order():
+    s1 = setsy([8,6,4,3,5,7,2,1])
+    assert s1.iter == [8,6,4,3,5,7,2,1]
