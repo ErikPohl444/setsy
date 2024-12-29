75a85cb54d9ce145a2dd96d0b78f2a3ce45d9513
Erik Pohl <epohl@dmigs.com>
2024-12-28 22:41:59 -0500

    added a powerset test

---
+++ tests/test_setsy.py
@@ -0,0 +1,23 @@
+from src.setsy import setsy
+import pytest
+
+
+def test_one():
+    x = setsy(["1", "2", "3"])
+    y = x.powerset()
+    expected = [
+                ('1', '2'),
+                ('3',),
+                ('1',),
+                ('3', '2'),
+                ('2',),
+                ('1', '3'),
+                ('1', '3', '2'),
+                ()
+            ]
+    for item in y:
+        print(item)
+        assert set(item) in [
+            set(n) for n in expected
+        ]
+    assert len(y) == len(expected)
